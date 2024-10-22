d3f4927f0aa7043142e929804907e2e33c4550ca
TJ <trevorebeckham@gmail.com>
2024-10-21 20:31:51 -0400

    - Testing Junk Gravity

---
+++ DWF24 - Team 15/Assets/Scripts/Zerogravity.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zerogravity : MonoBehaviour
+{
+
+    public Rigidbody2D rbd2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbd2 = GetComponent<Rigidbody2D>();
+        rbd2.gravityScale = 0f;
+        rbd2.drag = 0f;
+        rbd2.angularDrag = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
