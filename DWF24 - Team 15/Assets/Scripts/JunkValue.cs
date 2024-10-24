116ae0dcd188075e08ca554068413eb1cde8d8fb
Trevor (TJ) <trevorebeckham@gmail.com>
2024-10-24 13:19:33 -0400

    - fixed grabber code (ish)

---
+++ DWF24 - Team 15/Assets/Scripts/JunkValue.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JunkValue : MonoBehaviour
+{
+
+    public int junkValue = 0;
+    // Start is called before the first frame update
+}
--- DWF24 - Team 15/Assets/Scripts/Junk_Bin.cs
+++ DWF24 - Team 15/Assets/Scripts/Junk_Bin.cs
@@ -6,54 +6,39 @@ using UnityEngine.UI;
 
 public class Junk_Bin : MonoBehaviour
 {
-    //Total items collected
-    int junkCollected = 0;
-    public Text junkCollectedText;
-    
-    //Total player score
-    int totalScore = 0;
-    public Text totalScoreText;
 
-    //Value for each level of item, set in engine to desired value
-    public int smallValue = 250;
-    public int medValue = 500;
-    public int largeValue = 750;
-    public int legendaryValue = 1000;
+    public Grab_Function grabber;
+    public JunkValue Value;
+    public int junkCollected = 0;
+    public Text junkCollectedText;
+    public int junkScore = 0;
+    public Text junkScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grabber = GameObject.Find("Grabber").GetComponent<Grab_Function>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        //Display text to screen
         junkCollectedText.text = "Junk Collected: " + junkCollected.ToString();
-        totalScoreText.text = "Score: " + totalScore.ToString();
+        junkScoreText.text = "Score: " + junkScore.ToString();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //Check the tag of each item we bring to the score zone, add the corresponding value to the total score, increase the count of items collected and destroy the gameobject
-        if (collision.gameObject.CompareTag("Small Junk") == true)
+        Value = collision.gameObject.GetComponent<JunkValue>();
+
+        if (collision.gameObject.CompareTag("Grabbable") == true && grabber.isHolding == true)
         {
-           totalScore = totalScore + smallValue;
-           junkCollected++;
-           Destroy(collision.gameObject);
-        }
-        if (collision.gameObject.CompareTag("Medium Junk") == true)
-        {
-            totalScore = totalScore + medValue;
+
+            Destroy(grabber.grabber.transform.GetChild(0).gameObject);
+            grabber.isHolding = false;
+            junkScore += Value.junkValue;
             junkCollected++;
-            Destroy(collision.gameObject);
-        }
-        if (collision.gameObject.CompareTag("Large Junk") == true)
-        {
-            totalScore = totalScore + largeValue;
-            junkCollected++;
-            Destroy(collision.gameObject);
-        }
-        if (collision.gameObject.CompareTag("Legendary Junk") == true)
-        {
-            totalScore = totalScore + legendaryValue;
-            junkCollected++;
-            Destroy(collision.gameObject);
+
         }
     }
 }
